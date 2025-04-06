fa167be330d118892aff51cb93e6c96882cb795e
chen <1343744987@qq.com>
2025-04-06 16:06:03 +0800

    gin脚手架+配置文件

--- config/config.go
+++ config/config.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"gin--/internal/utils/logger"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"os"
+)
+
 type LogConfig struct {
 	Level      string `json:"level"`       // 日志等级
 	Filename   string `json:"filename"`    // 基准日志文件名
@@ -14,4 +21,55 @@ var Cfg = &LogConfig{
 	MaxSize:    100,
 	MaxAge:     30,
 	MaxBackups: 10,
+}
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Name     string
+	driver   string
+}
+
+type Config struct {
+	AppName  string
+	AppPort  int
+	DBConfig DBConfig
+}
+
+func ConfigInit() {
+	path, _ := os.Getwd()
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+
+	// 从配置文件读取配置
+	if err := viper.ReadInConfig(); err != nil {
+		logger.Logger.Error("读取配置文件失败", zap.Error(err))
+		panic(err)
+	}
+	// 从环境变量读取配置
+
+	viper.SetEnvPrefix("app") // 读取APP_前缀的环境变量
+	viper.AutomaticEnv()
+
+}
+
+// 暂时没用
+
+func GetConfig() Config {
+	ConfigInit()
+	config := Config{
+		AppName: viper.GetString("app.name"),
+		AppPort: viper.GetInt("app.port"),
+		DBConfig: DBConfig{
+			Host:     viper.GetString("database.host"),
+			Port:     viper.GetInt("database.port"),
+			User:     viper.GetString("database.username"),
+			Password: viper.GetString("database.password"),
+			Name:     viper.GetString("database.name"),
+			driver:   viper.GetString("database.driver"),
+		},
+	}
+	return config
 }
